9789d4f1ef729b93315d8022fd4a734f901f5bad
lopkiloinm <167727986+lopkiloinm@users.noreply.github.com>
2024-07-25 02:03:54 -0400

    Update computer.cc

--- computer.cc
+++ computer.cc
@@ -9,6 +9,113 @@ int Computer::getRandom(int low, int high) {
 	return (rd() - rd.min()) % (high - low + 1) + low;
 }
 
+const int P = 100, N = 320, B = 330, R = 500, Q = 900, K = 20000;
+
+// Array representation for point-square tables (evaluation matrices)
+const int pawn_table[8][8] = {
+    { 0,  0,  0,  0,  0,  0,  0,  0},
+    {50, 50, 50, 50, 50, 50, 50, 50},
+    {10, 10, 20, 30, 30, 20, 10, 10},
+    { 5,  5, 10, 25, 25, 10,  5,  5},
+    { 0,  0,  0, 20, 20,  0,  0,  0},
+    { 5, -5,-10,  0,  0,-10, -5,  5},
+    { 5, 10, 10,-20,-20, 10, 10,  5},
+    { 0,  0,  0,  0,  0,  0,  0,  0}
+};
+
+const int knight_table[8][8] = {
+    {-50,-40,-30,-30,-30,-30,-40,-50},
+    {-40,-20,  0,  0,  0,  0,-20,-40},
+    {-30,  0, 10, 15, 15, 10,  0,-30},
+    {-30,  5, 15, 20, 20, 15,  5,-30},
+    {-30,  0, 15, 20, 20, 15,  0,-30},
+    {-30,  5, 10, 15, 15, 10,  5,-30},
+    {-40,-20,  0,  5,  5,  0,-20,-40},
+    {-50,-40,-30,-30,-30,-30,-40,-50}
+};
+
+const int bishop_table[8][8] = {
+    {-20,-10,-10,-10,-10,-10,-10,-20},
+    {-10,  0,  0,  0,  0,  0,  0,-10},
+    {-10,  0,  5, 10, 10,  5,  0,-10},
+    {-10,  5,  5, 10, 10,  5,  5,-10},
+    {-10,  0, 10, 10, 10, 10,  0,-10},
+    {-10, 10, 10, 10, 10, 10, 10,-10},
+    {-10,  5,  0,  0,  0,  0,  5,-10},
+    {-20,-10,-10,-10,-10,-10,-10,-20}
+};
+
+const int rook_table[8][8] = {
+    { 0,  0,  0,  0,  0,  0,  0,  0},
+    { 5, 10, 10, 10, 10, 10, 10,  5},
+    {-5,  0,  0,  0,  0,  0,  0, -5},
+    {-5,  0,  0,  0,  0,  0,  0, -5},
+    {-5,  0,  0,  0,  0,  0,  0, -5},
+    {-5,  0,  0,  0,  0,  0,  0, -5},
+    {-5,  0,  0,  0,  0,  0,  0, -5},
+    { 0,  0,  0,  5,  5,  0,  0,  0}
+};
+
+const int queen_table[8][8] = {
+    {-20,-10,-10, -5, -5,-10,-10,-20},
+    {-10,  0,  0,  0,  0,  0,  0,-10},
+    {-10,  0,  5,  5,  5,  5,  0,-10},
+    { -5,  0,  5,  5,  5,  5,  0, -5},
+    {  0,  0,  5,  5,  5,  5,  0, -5},
+    {-10,  5,  5,  5,  5,  5,  0,-10},
+    {-10,  0,  5,  0,  0,  0,  0,-10},
+    {-20,-10,-10, -5, -5,-10,-10,-20}
+};
+
+const int king_table[8][8] = {
+    {-30,-40,-40,-50,-50,-40,-40,-30},
+    {-30,-40,-40,-50,-50,-40,-40,-30},
+    {-30,-40,-40,-50,-50,-40,-40,-30},
+    {-30,-40,-40,-50,-50,-40,-40,-30},
+    {-20,-30,-30,-40,-40,-30,-30,-20},
+    {-10,-20,-20,-20,-20,-20,-20,-10},
+    { 20, 20,  0,  0,  0,  0, 20, 20},
+    { 20, 30, 10,  0,  0, 10, 30, 20}
+};
+
+int getPieceValue(shared_ptr<ChessPiece> piece) {
+    if (!piece) return 0;
+    switch (piece->getType()) {
+        case ChessPiece::PAWN: return P;
+        case ChessPiece::KNIGHT: return N;
+        case ChessPiece::BISHOP: return B;
+        case ChessPiece::ROOK: return R;
+        case ChessPiece::QUEEN: return Q;
+        case ChessPiece::KING: return K;
+        default: return 0;
+    }
+}
+
+int getPieceSquareValue(shared_ptr<ChessPiece> piece, int row, int col) {
+    if (!piece) return 0;
+    int score = 0;
+    switch (piece->getType()) {
+        case ChessPiece::PAWN: score = pawn_table[row][col]; break;
+        case ChessPiece::KNIGHT: score = knight_table[row][col]; break;
+        case ChessPiece::BISHOP: score = bishop_table[row][col]; break;
+        case ChessPiece::ROOK: score = rook_table[row][col]; break;
+        case ChessPiece::QUEEN: score = queen_table[row][col]; break;
+        case ChessPiece::KING: score = king_table[row][col]; break;
+        default: break;
+    }
+    return piece->getColor() == Player::WHITE ? score : -score;
+}
+
+int evaluateBoard(shared_ptr<ChessBoard> board) {
+    int score = 0;
+    for (auto& piece : *board) {
+        int row = piece->getRow();
+        int col = piece->getCol();
+        score += getPieceValue(piece) + getPieceSquareValue(piece, row, col);
+    }
+    return score;
+}
+
 vector<int> Computer::getNextMove() {
 	vector<int> move;
 	auto real_board = board.lock();
